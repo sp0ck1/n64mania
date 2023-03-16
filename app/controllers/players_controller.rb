d0c9ffc2342da3442a73e07dbf0aa301d6ce0bbd
Cory Pettit <pettit1c@gmail.com>
2023-03-15 23:09:39 -0400

    Touch up styles for readqability in alpha.
    Added additional headers

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,7 +1,7 @@
 class PlayersController < ApplicationController
 
   def index
-    @players = Player.all
+    @players = Player.all.sort_by(&:name)
   end
   
   def show
@@ -10,6 +10,7 @@ class PlayersController < ApplicationController
     @placements_including_player = Placement.where(player_id: @player.id)
     @races = []
     @placements_including_player.each { |placement| @races << Race.find_by_id(placement.race_id) }
+    @races.sort_by(&:date)
     
   end
 
