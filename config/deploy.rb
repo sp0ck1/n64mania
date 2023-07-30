3e127e54d5729d2d57eedd94a8a3ba3e42944865
Cory Pettit <pettit1c@gmail.com>
2023-07-30 00:21:24 -0400

    Fix comma in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :repo_url, "git@github.com:sp0ck1/n64mania.git"
 
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/master.key, config/application.yml}
+set :linked_files, %w{config/database.yml config/master.key config/application.yml}
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "public/uploads", "vendor", "storage"
