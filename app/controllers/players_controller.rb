149eff337a9e7bfd620e78fe918b9f4a41533b84
Cory Pettit <pettit1c@gmail.com>
2023-07-30 07:01:28 -0400

    Cleanup bad spacing and repeated code

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,7 +1,6 @@
 class PlayersController < ApplicationController
 
   def index
-    player_names = []
     @players = Player.all
     
     @players.each do |p|
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,4 @@
 class Player < ActiveRecord::Base
-
-
-
     has_many :comments, :class_name => 'Comment'
     has_many :placements, :class_name => 'Placement'
 end
