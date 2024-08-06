777d2ca7a4c400b9f29ceeb5fcc22da726575fa3
Cory Pettit <pettit1c@gmail.com>
2024-08-06 18:24:51 -0400

    Add Genres list to header

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -3,7 +3,7 @@ class GenresController < ApplicationController
 
   # GET /genres or /genres.json
   def index
-    @genres = Genre.all
+    @genres = Genre.all.sort_by {|n| n.name }
   end
 
   # GET /genres/1 or /genres/1.json
